6c985076f977b17cf142b6d9e5b26260aa8419bf
siudeks <slawomir@siudek.net>
2023-11-10 19:33:26 +0100

    New code

--- calc/src/main/java/com/example/calc1/CalculatorView.java
+++ calc/src/main/java/com/example/calc1/CalculatorView.java
@@ -1,7 +1,8 @@
 package com.example.calc1;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
@@ -9,15 +10,31 @@ import com.vaadin.flow.router.Route;
 @Route("")
 public class CalculatorView extends VerticalLayout {
 
-    private TextField input;
+    private TextField inputLabel;
+    private TextField inputAsInches;
     private TextField display2;
 
     public CalculatorView() {
-        input = new TextField();
+
+
+        inputLabel = new TextField();
+        inputLabel.setReadOnly(true);
+        inputLabel.setValue("Cale");
+
+        inputAsInches = new TextField();
         // display1.setReadOnly(true);
-        input.setWidth("200px");
-        input.addValueChangeListener(it -> {
-            display2.setValue(it.getValue());
+        inputAsInches.setWidth("200px");
+        inputAsInches.addValueChangeListener(it -> {
+
+            try {
+                var valueAsInches = it.getValue();
+                var valueAsInchesAsInt = Double.parseDouble(valueAsInches);
+                var valueAsMm = valueAsInchesAsInt * 25.4;
+                display2.setValue(Double.toString(valueAsMm));
+            } catch (Exception ex) {
+                display2.setValue(ex.getMessage());
+            }
+
         });
 
         display2 = new TextField();
@@ -25,7 +42,22 @@ public class CalculatorView extends VerticalLayout {
         display2.setValue("0");
         display2.setWidth("200px");
 
-        add(input, display2);
+        var layout1 = new HorizontalLayout();
+        layout1.setPadding(true);
+        layout1.setWidth("auto");
+        layout1.setMargin(true);
+        layout1.setAlignItems(FlexComponent.Alignment.STRETCH);
+        layout1.add(inputLabel);
+        layout1.add(inputAsInches);
+
+        var layout2 = new HorizontalLayout();
+        layout2.setPadding(true);
+        layout2.setWidth("auto");
+        layout2.setMargin(true);
+        layout2.setAlignItems(FlexComponent.Alignment.STRETCH);
+        layout2.add(display2);
+
+        add(layout1, layout2);
 
         setSpacing(true);
     }
