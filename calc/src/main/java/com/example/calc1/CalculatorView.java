f5879b3143129cf3eda42c6e272222f367a76149
siudeks <slawomir@siudek.net>
2023-11-14 18:30:30 +0100

    Add milis

--- calc/src/main/java/com/example/calc1/CalculatorView.java
+++ calc/src/main/java/com/example/calc1/CalculatorView.java
@@ -1,6 +1,5 @@
 package com.example.calc1;
 
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -10,19 +9,14 @@ import com.vaadin.flow.router.Route;
 @Route("")
 public class CalculatorView extends VerticalLayout {
 
-    private TextField inputLabel;
-    private TextField inputAsInches;
-    private TextField display2;
-
     public CalculatorView() {
 
+        var result = new TextField();
+        result.setReadOnly(true);
+        result.setValue("0");
+        result.setWidth("200px");
 
-        inputLabel = new TextField();
-        inputLabel.setReadOnly(true);
-        inputLabel.setValue("Cale");
-
-        inputAsInches = new TextField();
-        // display1.setReadOnly(true);
+        var inputAsInches = new TextField();
         inputAsInches.setWidth("200px");
         inputAsInches.addValueChangeListener(it -> {
 
@@ -30,35 +24,63 @@ public class CalculatorView extends VerticalLayout {
                 var valueAsInches = it.getValue();
                 var valueAsInchesAsInt = Double.parseDouble(valueAsInches);
                 var valueAsMm = valueAsInchesAsInt * 25.4;
-                display2.setValue(Double.toString(valueAsMm));
+                result.setValue(Double.toString(valueAsMm));
             } catch (Exception ex) {
-                display2.setValue(ex.getMessage());
+                result.setValue(ex.getMessage());
             }
 
         });
 
-        display2 = new TextField();
-        display2.setReadOnly(true);
-        display2.setValue("0");
-        display2.setWidth("200px");
 
-        var layout1 = new HorizontalLayout();
-        layout1.setPadding(true);
-        layout1.setWidth("auto");
-        layout1.setMargin(true);
-        layout1.setAlignItems(FlexComponent.Alignment.STRETCH);
-        layout1.add(inputLabel);
+        var inputLabelInches = newLabel("Cale");
+        var layout1 = newLayout();
+        layout1.add(inputLabelInches);
         layout1.add(inputAsInches);
 
-        var layout2 = new HorizontalLayout();
-        layout2.setPadding(true);
-        layout2.setWidth("auto");
-        layout2.setMargin(true);
-        layout2.setAlignItems(FlexComponent.Alignment.STRETCH);
-        layout2.add(display2);
 
-        add(layout1, layout2);
+        var inputLabelMilis = newLabel("Milimetry");
+        var inputAsMilis = newInput();
+        inputAsMilis.addValueChangeListener(it -> {
+            try {
+                var valueAsMilis = it.getValue();
+                var valueAsMilisAsInt = Double.parseDouble(valueAsMilis);
+                var valueAsMm = valueAsMilisAsInt / 25.4;
+                result.setValue(Double.toString(valueAsMm));
+            } catch (Exception ex) {
+                result.setValue(ex.getMessage());
+            }
+
+        });
+        var layout2 = newLayout();
+        layout2.add(inputLabelMilis, inputAsMilis);
+
+        var layout3 = newLayout();
+        layout3.add(result);
+
+        add(layout1, layout2, layout3);
 
         setSpacing(true);
+    }
+
+    private static HorizontalLayout newLayout() {
+        var result = new HorizontalLayout();
+        result.setPadding(true);
+        result.setWidth("auto");
+        result.setMargin(true);
+        result.setAlignItems(FlexComponent.Alignment.STRETCH);
+        return result;
+    }
+
+    private static TextField newLabel(String text) {
+        var result = new TextField();
+        result.setReadOnly(true);
+        result.setValue(text);
+        return result;
+    }
+
+    private static TextField newInput() {
+        var result = new TextField();
+        result.setWidth("200px");
+        return result;
     }
 }
