b9dc9a81acb7e56f5bc2642d77b6ebd06daaefd8
siudeks <slawomir@siudek.net>
2023-11-15 18:02:29 +0100

    calc1

---
+++ thymeleaf-initial/src/main/java/com/example/demo/CalcController.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class CalcController {
+
+    // inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
+
+    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("project", new Project());
+
+        return "calc"; // view
+    }
+
+    // /hello?name=kotlin
+    @GetMapping("/hello")
+    public String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "calc"; // view
+    }
+
+    @PostMapping("/save-project")
+    public String saveProjectSubmission(@ModelAttribute Project project) {
+
+        // TODO: save project in DB here
+
+        return "result";
+    }
+    
+
+}
